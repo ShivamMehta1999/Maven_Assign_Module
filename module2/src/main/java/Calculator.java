d66c0acd43bb7fd0f0a32fe09a6517e4f3b8bbd6
ShivamMehta1999 <shivam.mehta@knoldus.com>
2022-02-09 00:17:02 +0530

    Maven Multi-Module Assignment

---
+++ module2/src/main/java/Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+    void addition( int no1, int no2) {
+        int add;
+        add = no1 + no2;
+
+        System.out.println("Addition is: " +add);
+
+    }
+
+    void subtract(int no1, int no2) {
+        int sub;
+        sub = no1 - no2;
+
+        System.out.println("Subtract is: " +sub);
+    }
+
+    void multiplication(int no1, int no2) {
+        int multiply;
+        multiply = no1 * no2;
+
+        System.out.println("Multiplication is: " +multiply);
+    }
+
+    void division(float no1, float no2) {
+        float division;
+        division = no1 / no2;
+
+        System.out.println("Division is: " +division);
+    }
+}
